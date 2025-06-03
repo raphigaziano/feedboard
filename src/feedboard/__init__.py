3b9526410f23e3790dfbe69da8a1148b4bb86c2b
rga <r.gaziano@gmail.com>
2025-06-03 10:11:26 +0200

    Fleshing out feed generation:
    - Flesh out structure and fix some oversights
    - Added some docs and tests
    - Use a local copy of feeds instead of re-downloading them on each test
      run
    - Add merge_feeds function

--- src/feedboard/__init__.py
+++ src/feedboard/__init__.py
@@ -1,2 +1,19 @@
+from feedboard.feed import get_all_feeds
+
+
+FEED_URLS = {
+    'Tech': [
+        'tests/data/feeds/ploum.xml',
+        'tests/data/feeds/pocoo.xml',
+    ],
+    'My crap': [
+        'tests/data/feeds/rgaz.xml',
+    ],
+}
+
+
 def main() -> None:
-    print("Hello from feedboard!")
+    for cat, entries in get_all_feeds(FEED_URLS).items():
+        print(cat)
+        for e in entries:
+            print('\t', e.feed.title, e.published, e.title, e.link)
--- src/feedboard/feed.py
+++ src/feedboard/feed.py
@@ -1,20 +1,19 @@
-import feedparser
+import itertools
 
-FEED_URLS = {
-    'Tech': [
-        'https://ploum.net/atom.xml',
-        'https://lucumr.pocoo.org/feed.atom',
-    ],
-    'My crap': [
-        'https://www.rgaz.fr/feeds/atom.xml',
-    ],
-}
+import feedparser
 
 
 class Entry:
+    """
+    A fully parsed RSS / Atom feed.
 
-    def __init__(self, feed_title, raw_data: dict):
-        self.feed_title = feed_title
+    This is a simple wrapper around a FeedParserDict, with shortcuts to the
+    most commonly accessed fields.
+
+    """
+
+    def __init__(self, feed, raw_data: dict):
+        self.feed = feed
         self.raw_data = raw_data
 
     @property
@@ -22,11 +21,27 @@ class Entry:
         return self.raw_data['title']
 
     @property
+    def link(self):
+        return self.raw_data['link']
+
+    @property
     def published(self):
-        return self.raw_data.get('published', self.raw_data['updated'])
+        """
+        Will fallback on the `updated` field if `published` is not present.
+        Return None if nothing can be found.
+        """
+        return self.raw_data.get(
+            'published_parsed', self.raw_data.get('updated_parsed'))
 
 
 class Feed:
+    """
+    An individual Feed Entry.
+
+    This is a simple wrapper around a FeedParserDict, with shortcuts to the
+    most commonly accessed fields.
+
+    """
 
     def __init__(self, data):
         self.data = data
@@ -37,8 +52,8 @@ class Feed:
 
     @property
     def entries(self):
-        for e in self.data.entries:
-            yield Entry(self.title, e)
+        for e in self.data['entries']:
+            yield Entry(self, e)
 
     @classmethod
     def from_url(cls, url):
@@ -46,5 +61,26 @@ class Feed:
         return cls(parsed_feed)
 
 
-def get_feed(feed_url):
-    return Feed.from_url(feed_url)
+def merge_feeds(*feeds):
+    """
+    Return a regular list of entries from all the passed feeds, sorted by
+    descending date of publication.
+
+    """
+    yield from sorted(
+        itertools.chain.from_iterable(f.entries for f in feeds),
+        key=lambda e: e.published,
+        reverse=True
+    )
+
+
+def get_all_feeds(feed_config):
+    """
+    Retrieve all feeds from the passed config dict and aggregate all their
+    entries, keeping them mapped to their respective categories.
+
+    """
+    r = {}
+    for category, feed_urls in feed_config.items():
+        r[category] = merge_feeds(*[Feed.from_url(url) for url in feed_urls])
+    return r
